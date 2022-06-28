37ec7648b61afa178babcaf7404863996c8250d4
Santisiele <santi.siele@gmail.com>
2022-06-27 22:22:26 -0300

    api

--- src/services/noticiaService.js
+++ src/services/noticiaService.js
@@ -7,34 +7,34 @@ const tablaEquipo=process.env.DB_TABLA_equipo;
 const tablaInter=process.env.DB_TABLA_intermedia;
 
 export class noticiaService {
-    getAllNoticia = async() => {
-        console.log('Get all');
+    getAllNoticia = async(idEquipo) => {
         let response = 0;
+        let where = false;
         let query = `SELECT * FROM ${tablaNotcia}`
+        let query1 = ``
+        let query2=``
 
         response= await dbHelperAll({},query);
         return response.recordset;
     }
 
-    getpersonajeById = async(id) => {
-        console.log('Get by ID');
-        const query = `SELECT * FROM ${tablaPersonaje} WHERE id=@id`;
-        const query2 = `SELECT PeliSerie.* FROM ${tablaPeliSerie}, ${tablaPersonaje} , ${tablaInter} WHERE ${tablaPeliSerie}.id = ${tablaInter}.idPeliSerie and ${tablaPersonaje}.id = ${tablaInter}.idPersonaje and ${tablaPersonaje}.id = @id`
-        const personaje = await dbHelperAll(id, undefined, query);
-        const PeliSerie = await dbHelperPeliSerie(id, undefined, query2);
-        personaje.recordset[0].PeliculasSeries=PeliSerie.recordset;
-        return personaje.recordset[0];
+    getnoticiaById = async(id) => {
+        const query = `SELECT * FROM ${tablaNotcia} WHERE id=@id`;
+        const query2 = `SELECT equipo.* FROM ${tablaEquipo}, ${tablaNotcia} , ${tablaInter} WHERE ${tablaEquipo}.idEquipo = ${tablaInter}.idEquipo and ${tablaNotcia}.idNoticia = ${tablaInter}.idNoticia and ${tablaNotcia}.idNoticia = @id`
+        const noticia = await dbHelperAll({id}, query);
+        const equipo = await dbHelperPeliSerie({id}, query2);
+        noticia.recordset[0].Equipo=equipo.recordset;
+        return noticia.recordset[0];
     }
 
-    createpersonaje = async(personaje) => {
-        console.log('Create');
+    createnoticia = async(noticia) => {
         const query = `INSERT INTO ${tablaPersonaje}(imagen, nombre, edad, peso, historia, comidaFavorita) VALUES (@imagen, @nombre, @edad, @peso, @historia, @comidaFavorita)`
         const response = await dbHelperAll(undefined, personaje, query);
 
         return response.recordset;
     }
 
-    updatepersonajeById = async(id, personaje) => {
+    updatenoticiaById = async(id, noticia) => {
         console.log('Update by ID');
         const query = `UPDATE ${tablaPersonaje} SET imagen = @imagen, nombre = @nombre, edad = @edad, peso = @peso, historia = @historia, comidaFavorita = @comidaFavorita WHERE id = @Id`
         const response = await dbHelperAll(id, personaje, query);
@@ -42,10 +42,9 @@ export class noticiaService {
         return response.recordset;
     }
 
-    deletepersonajeById = async(id) => {
-        console.log('Delete by ID');
-        const query = `DELETE FROM ${tablaPersonaje} WHERE id = @id`
-        const response = await dbHelperAll(id, undefined, query);
+    deletenoticiaById = async(id) => {
+        const query = `DELETE FROM ${tablaNotcia} WHERE id = @id`
+        const response = await dbHelperAll({id}, query);
         return response.recordset;
     }
 }
